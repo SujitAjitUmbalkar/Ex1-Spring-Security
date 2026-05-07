6e98a46fcedfe8770c3b04dc0502183e72dfaa42
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-05-07 12:19:34 +0530

    Notes of previous commit added

--- SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/services/JwtService.java
+++ SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/services/JwtService.java
@@ -35,3 +35,35 @@ public class JwtService
                 .compact();
     }
 }
+
+//**1. `@Service`** → Spring Boot creates object(bean) of `JwtService` class automatically.
+//
+//**2. `@Value("${jwt.secretKey}")`** → Reads secret key value from `application.properties`.
+//
+//**3. `jwtToken` variable** → Stores secret key string from properties file.
+//
+//**4. `generateToken(User user)` method called** → Starts JWT token creation process.
+//
+//**5. `Jwts.builder()`** → Creates JWT builder object for building token.
+//
+//**6. `.setSubject(user.getId().toString())`** → Stores user id inside token as subject.
+//
+//**7. `.claim("email", user.getEmail())`** → Adds custom email data inside token.
+//
+//**8. `.claim("roles", Set.of("ADMIN","USER"))`** → Adds user roles inside token payload.
+//
+//**9. `.setIssuedAt(new Date())`** → Stores current login time in token.
+//
+//**10. `.setExpiration(...)`** → Sets token expiry time to 1 hour.
+//
+//**11. `getSecretKey()` method called** → Generates secret signing key for JWT.
+//
+//**12. `jwtToken.getBytes(StandardCharsets.UTF_8)`** → Converts secret string into byte array.
+//
+//**13. `Keys.hmacShaKeyFor(...)`** → Creates `SecretKey` object using HMAC algorithm.
+//
+//**14. `.signWith(getSecretKey())`** → Digitally signs token using secret key.
+//
+//**15. `.compact()`** → Converts JWT object into final encoded JWT string.
+//
+//**16. Final JWT token returned** → Token is sent back to client/user.git
