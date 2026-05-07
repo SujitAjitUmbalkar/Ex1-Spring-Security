d5b9e3495c059f264b9dd27758d07282f35cab11
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-05-07 15:40:40 +0530

    5.6 Signup and Login using JWT

---
+++ SecurityApplication/src/main/java/com/codingshuttle/SecurityApp/SecurityApplication/controllers/AuthController.java
@@ -0,0 +1,12 @@
+package com.codingshuttle.SecurityApp.SecurityApplication.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController
+{
+
+
+}
